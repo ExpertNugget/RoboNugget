e6ec17020faed44793fbdbbedbcb7c76a5b6cecc
ExpertNugget <deaconjf0427@outlook.com>
2023-09-05 09:16:07 -0600

    update

--- cogs/download.py
+++ cogs/download.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import toml
 import ftplib
 import json
+from datetime import datetime, timezone
+from mediafire import (MediaFireApi, MediaFireUploader)
 
 
 class download(commands.Cog):  # create a class for our cog that inherits from commands.Cog
@@ -30,8 +32,38 @@ class download(commands.Cog):  # create a class for our cog that inherits from c
         with open('./data/backups.json', 'r') as f:
             raw_data = json.load(f)
         data = raw_data['backups']
-        print(data)
-        await ctx.respond('potatoes')
+        createtimes = []
+        for backups in data:
+            createtimes.append(backups['createTime'])
+        for backups in data:
+            if backups['createTime'] == max(createtimes):
+                createtime = max(createtimes)
+                timestamp = createtime // 1000
+                dt = datetime.fromtimestamp(timestamp)
+                dt_utc = dt.astimezone(timezone.utc)
+                filename = dt_utc.strftime('%Y-%-m-%-d_%-H-%-M-%-S')
+                file = './backups/' + filename + '.zip'
+                with open('./data/backup.zip', "wb") as f:
+                    ftp.retrbinary(f"RETR {file}", f.write)
+                with open("config.toml", "r") as f:
+                    data = toml.load(f)
+                api = MediaFireApi()
+                session = api.user_get_session_token(
+                    email=data['MediaFire']['email'],
+                    password=data['MediaFire']['password'],
+                    app_id=data['MediaFire']['app_id']
+                )
+                api.session = session
+                uploader = MediaFireUploader(api)
+                with open('./data/backup.zip', 'rb') as fd:
+                    result = uploader.upload(fd, 'Some filename.txt',
+                                             folder_key='1234567890123')
+                embed = discord.Embed(
+                    title=backups["worldName"],
+                    description=f'Creation Date: <t:{timestamp}:f> (<t:{timestamp}:R>)'
+                )
+
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
