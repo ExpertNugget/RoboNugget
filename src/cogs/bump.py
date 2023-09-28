e25a936676ddaf1ba5c345235b513cf774705575
ExpertNugget <deaconjf0427@outlook.com>
2023-09-27 21:53:21 -0600

    added debug prints

--- src/cogs/bump.py
+++ src/cogs/bump.py
@@ -10,16 +10,25 @@ class bump(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        #ignores non bots
+        if not message.author.bot:
+            return
         channel = message.channel
+        print('checking message author')
         if message.author == 302050872383242240:
+            print('author confirm to be disboard\nchecking embed')
             for embed in message.embeds:
+                print(embed)
                 if "Bump done!" in embed.description:
+                    print('embed matched')
                     embed = discord.Embed(
                         title="Thank you for bumping the server!",
                         description="I'll ping <@836263721281650718> when the server can be bumped again."
                     )
+                    
                     await channel.send(embed=embed)
                     # waits 2 hours and sends a followup
+                    print('waiting 2 hours for next response')
                     await asyncio.sleep(7200)
                     content = '<@836263721281650718>'
                     embed = discord.Embed(
@@ -27,5 +36,23 @@ class bump(commands.Cog):
                         description='Bump the server by running </bump:947088344167366698>'
                     )
                     await channel.send(content=content, embed=embed)
+                else:
+                    return
+        else:   
+            return
+
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # Ignore messages from non-bots
+        if not message.author.bot:
+            return
+
+        # Do something with the bot message
+        print(message.content)
+
 def setup(bot): 
     bot.add_cog(bump(bot))
