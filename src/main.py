6377054103fe1eec2e1c19cb45c13d556b267706
ExpertNugget <deaconjf0427@outlook.com>
2023-09-27 21:25:36 -0600

    moved token to database instead of toml

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 import os
 import discord
-import toml
 import sqlite3
 
-database = "src/data/mpsdb.sqlite3"
+# minor corrections for running dir, mainly for vsc
+if "/src" in os.getcwd():
+    pass
+else:
+    os.chdir('./src')
+
+
+database = "./data/mpsdb.sqlite3"
 
 
 with sqlite3.connect(database) as conn:
     cur = conn.cursor()
     # temp solution for data storage until main database is working
     cur.execute("""
-    
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         discord_id INTEGER NOT NULL
@@ -24,17 +29,14 @@ with sqlite3.connect(database) as conn:
         guild_id INTEGER NOT NULL
     )
     """)
+with sqlite3.connect(database) as conn:
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS token (
+        token TEXT PRIMARY KEY)
+    """)
 
 bot = discord.Bot()
-
-# Config template
-config = {'TOKEN': ''}
-if not os.path.isfile('src/data/config.toml'):
-    with open('src/data/config.toml', 'w') as f:
-        toml.dump(config, f)
-else:
-    pass
-
 
 @bot.event
 async def on_ready():
@@ -42,7 +44,7 @@ async def on_ready():
 
 
 # grabs all files ending in .py in src/cogs, and stores them in a list minus the .py to load all cogs.
-cog_list = [f[:-3] for f in os.listdir('src/cogs') if f.endswith('.py')]
+cog_list = [f[:-3] for f in os.listdir('./cogs') if f.endswith('.py')]
 
 # Cogs disabled due to no longer or not working code
 exclude_list = ['download', 'link', 'servers'] 
@@ -53,23 +55,27 @@ for cog in cog_list:
         continue
     bot.load_extension(f'cogs.{cog}')
 
+#add
+#with sqlite3.connect(database) as conn:
+#    cur = conn.cursor()
+#    cur.execute("INSERT INTO users (discord_id) VALUES (?)", (user.id,))
+#
+##get
+#get
 
 # keeps asking for a token until a valid token is provided
-def startbot():
-    with open("src/data/config.toml", "r") as f:
-        data = toml.load(f)
-    TOKEN = data['TOKEN']
-    if TOKEN == None:
-        TOKEN = data.get('TOKEN', input("Enter Discord bot token:"))
-    else:
-        pass
-    while True:
-        try:
-            bot.run(TOKEN)
-            data = {'TOKEN': TOKEN}
-            with open("src/data/config.toml", "w") as f:
-                toml.dump(data, f)
-            break
-        except:
-            TOKEN = input("Invalid token, enter Discord bot token:")
-startbot()
+while True:
+    try:
+        with sqlite3.connect(database) as conn:
+            cur = conn.cursor()
+            cur.execute("SELECT token FROM token")
+            TOKEN = cur.fetchone()  
+        bot.run(TOKEN[0])
+        break
+    except Exception as e:  # Catch all exceptions
+        print(f"An error occurred: {e}")
+        TOKEN = input("Invalid token, enter Discord bot token:")
+        with sqlite3.connect(database) as conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO token (token) VALUES (?)", (TOKEN,))
+
