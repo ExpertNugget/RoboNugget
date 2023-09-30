89948f34549bb776511e1d4e0b339149d9c071fc
ExpertNugget <deaconjf0427@outlook.com>
2023-09-29 18:12:55 -0600

    added config handling

--- src/cogs/bump.py
+++ src/cogs/bump.py
@@ -2,6 +2,9 @@
 import discord
 from discord.ext import commands
 import asyncio
+import sqlite3
+
+database = "./data/mpsdb.sqlite3"
 
 class bump(commands.Cog): 
 
@@ -17,23 +20,75 @@ class bump(commands.Cog):
         if message.author.id == 302050872383242240:
             for embed in message.embeds:
                 if "Bump done!" in embed.description:
-                    embed = discord.Embed(
-                        title="Thank you for bumping the server!",
-                        description="I'll ping <@&836263721281650718> when the server can be bumped again."
-                    )
-                    await channel.send(embed=embed)
+                    with sqlite3.connect(database) as conn:
+                        cur = conn.cursor()
+                        cur.execute("INSERT OR REPLACE INTO bumpconfig (guild_id) VALUES (?)", (message.guild.id,))
+                        cur.execute("SELECT * from bumpconfig WHERE guild_id = ?", (message.guild.id,))
+                        rows = cur.fetchall()
+                        column_names = [description[0] for description in cur.description]
+                        for row in rows:
+                            raw_data = dict(zip(column_names, row))
+                            is_embed = raw_data[is_embed]
+                            thank_title = raw_data[thank_title]
+                            thank_description = raw_data[thank_description]
+                            remind_description = raw_data[remind_description]
+                            remind_title = raw_data[remind_title]
+                            ping_role = raw_data[ping_role]
+                            role_id = raw_data[role_id]
+                    if is_embed == 1:
+                        if thank_title:
+                            embed = discord.Embed(
+                            title=thank_title,
+                            description=thank_description
+                        )
+                        else:
+                            embed = discord.Embed(
+                            description=thank_description
+                        )
+                    elif is_embed == 0:
+                        if thank_title:
+                            content = thank_title + thank_description
+                        else:
+                            content = thank_description
+                    
+                    
+                    await channel.send(content=content, embed=embed)
                     # waits 2 hours and sends a followup
                     await asyncio.sleep(7200)
-                    content = '<@&36263721281650718>'
-                    embed = discord.Embed(
-                        title='It\'s time to bump!',
-                        description='Bump the server by running </bump:947088344167366698>'
-                    )
+
+                    if is_embed == 0 and ping_role == 0:
+                        if remind_title:
+                            content = remind_title + "\n" + remind_description
+                        else: 
+                            content = remind_description
+                    elif is_embed == 0 and ping_role == 1:
+                        if remind_title:
+                            content = f"<@&{role_id}>" + "\n" + remind_title + "\n" + remind_description
+                        else: 
+                            content = f"<@&{role_id}>" + "\n" + remind_description
+                    elif is_embed == 1 and ping_role == 1:
+                        if remind_title:
+                            embed = discord.Embed(
+                                title=remind_title,
+                                description=remind_description
+                            )
+                            content = f"<@&{role_id}>"
+                        else:
+                            embed = discord.Embed(
+                                description=remind_description
+                            )
+                            content = f"<@&{role_id}>"
+                    elif is_embed == 1 and ping_role == 0:
+                        if remind_title:
+                            embed = discord.Embed(
+                                title=remind_title,
+                                description=remind_description
+                            )
+                        else:
+                            embed = discord.Embed(
+                                description=remind_description
+                            )
                     await channel.send(content=content, embed=embed)
-                else:
-                    return
-        else:   
-            return
 
 def setup(bot): 
     bot.add_cog(bump(bot))
