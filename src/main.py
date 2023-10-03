c44ae64b7b6c4c2221df3eea92b3a273a1f29594
ExpertNugget <deaconjf0427@outlook.com>
2023-10-02 20:05:07 -0600

    updated tables

--- src/main.py
+++ src/main.py
@@ -11,21 +11,23 @@ else:
 
 database = "./data/mpsdb.sqlite3"
 
-
 with sqlite3.connect(database) as conn:
     cur = conn.cursor()
     # temp solution for data storage until main database is working
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        discord_id INTEGER NOT NULL
-    )
+        discord_id INTEGER NOT NULL,
+        username TEXT,
+        log_thread_id INTEGER
+    );
     """)
 with sqlite3.connect(database) as conn:
     cur = conn.cursor()
     cur.execute("""
     CREATE TABLE IF NOT EXISTS token (
-        token TEXT PRIMARY KEY)
+        token TEXT PRIMARY KEY
+    );
     """)
 with sqlite3.connect(database) as conn:
     cur = conn.cursor()
@@ -36,18 +38,28 @@ with sqlite3.connect(database) as conn:
         role_id INTEGER,
         ping_role INTEGER DEFAULT "0" CHECK (ping_role IN (0,1)) NOT NULL,
         thank_title TEXT DEFAULT "Thanks for bumping the server!",
-        thank_description TEXT DEFAULT "I'll ping you when the next bump is ready. I'll remind again when the next bump is available {next-bump-count} " NOT NULL,
+        thank_description TEXT DEFAULT "I'll ping you when the next bump is ready. I'll remind again when the next bump is available {next-bump-count}" NOT NULL,
         remind_title TEXT DEFAULT "It's time to bump!",
         remind_description TEXT DEFAULT "Bump the server by running </bump:947088344167366698>" NOT NULL
-    )
+    );
     """)
 with sqlite3.connect(database) as conn:
     cur = conn.cursor()
     cur.execute("""
     CREATE TABLE IF NOT EXISTS configs (
         guild_id INTEGER PRIMARY KEY NOT NULL,
+        staff_role_id INTEGER,
+        log_channel_id INTEGER,
+        log_channel_webhook TEXT
+    );
+    """)
+with sqlite3.connect(database) as conn:
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS info (
+        guild_id INTEGER PRIMARY KEY NOT NULL,
         staff_role_id INTEGER
-    )
+    );
     """)
 
 
@@ -76,7 +88,7 @@ while True:
     try:
         with sqlite3.connect(database) as conn:
             cur = conn.cursor()
-            cur.execute("SELECT token FROM token")
+            cur.execute("SELECT token FROM token;")
             TOKEN = cur.fetchone()  
         bot.run(TOKEN[0])
         break
@@ -85,5 +97,5 @@ while True:
         TOKEN = input("Invalid token, enter Discord bot token:")
         with sqlite3.connect(database) as conn:
             cur = conn.cursor()
-            cur.execute("INSERT INTO token (token) VALUES (?)", (TOKEN,))
+            cur.execute("INSERT INTO token (token) VALUES (?);", (TOKEN,))
 
