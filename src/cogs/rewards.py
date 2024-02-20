8e2d3e9ec0c8119360e9fdcaa77747a830b27100
ExpertNugget <deaconjf04@gmail.com>
2024-02-20 18:39:33 +0000

    unfinished but my brain hurts

--- src/cogs/rewards.py
+++ src/cogs/rewards.py
@@ -1,10 +1,36 @@
 import discord
 from discord.ext import commands
+from functions import fetchData
+from config import databaseURL
+from firebase_admin import db
 
 
 class rewards(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return None
+        ref = db.reference(
+            path=f"/{message.guild.id}/{message.author.id}/userData/",
+        )
+        userData = fetchData(
+            path=f"/{message.guild.id}/{message.author.id}/userData/",
+            cache="rewardsCache",
+        )
+        if not userData:
+            data = {
+                "points": 0,
+            }
+            ref.set(data)
+        points = int(userData["points"])
+        points += 1
+        data = {
+            "points": str(points),
+        }
+        ref.set(data)
 
 
 def setup(bot):
