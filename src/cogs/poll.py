5b7a46ee63d8502da445bb342bae2a13da308a78
ExpertNugget <deaconjf04@gmail.com>
2025-02-07 09:50:56 -0700

    boiler code added

--- src/cogs/poll.py
+++ src/cogs/poll.py
@@ -2,11 +2,11 @@ import discord
 from discord.ext import commands
 
 
-class stream(commands.Cog):
+class poll(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
 
 def setup(bot):
-    bot.add_cog(stream(bot))
+    bot.add_cog(poll(bot))
