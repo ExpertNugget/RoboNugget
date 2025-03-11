19593f2c9fc265432690c09a03ecf4503404f2b2
ExpertNugget <deaconjf04@gmail.com>
2025-03-11 11:04:28 -0600

    smarter logic

--- src/cogs/hyperlinker.py
+++ src/cogs/hyperlinker.py
@@ -76,27 +76,24 @@ class hyperlinker(commands.Cog):
             hyperlink = f"[{label}]({url})"
             modified_content = modified_content.replace(url, hyperlink)
 
-        try:
-            await message.channel.create_webhook(name="Hyperlinker")
-        except:
-            # Webhook already exists, try to get it
-            webhooks = await message.channel.webhooks()
-            for webhook in webhooks:
-                if webhook.name == "Hyperlinker":
-                    webhook = webhook
+        webhooks = await message.channel.webhooks()
+        for webhook in webhooks:
+            if webhook.name == "Hyperlinker":
+                webhook = webhook
+        
+        if webhook is None:
+            webhook = await message.channel.create_webhook(name="Hyperlinker")
             
-        try:
-            async with aiohttp.ClientSession() as session:
-                webhook = Webhook.from_url("https://discord.com/api/webhooks/1170892019665731625/SvIEhmQdTh2srcno8G-RldAF90dpgMtye3neWkXEINnnwb_HNet_EUHz0lOnf-Q9LeVk", session=session)
-                await webhook.send(
-                    content=modified_content,
-                    username=message.author.display_name,
-                    avatar_url=message.author.display_avatar,
-                )
-        except:
-            print("Error sending message")
-        finally:
-            await message.delete()
+
+        async with aiohttp.ClientSession() as session:
+            webhook = Webhook.from_url("https://discord.com/api/webhooks/1170892019665731625/SvIEhmQdTh2srcno8G-RldAF90dpgMtye3neWkXEINnnwb_HNet_EUHz0lOnf-Q9LeVk", session=session)
+            await webhook.send(
+                content=modified_content,
+                username=message.author.display_name,
+                avatar_url=message.author.display_avatar,
+            )
+        
+        await message.delete()
 
 def setup(bot):
     bot.add_cog(hyperlinker(bot))
